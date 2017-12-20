bab2f7acfedcdada4891d97480ac196a8da2f148
dmemig <drew.emig@spglobal.com>
2017-12-20 13:23:47 -0700

    2nd com

--- demogit/src/demogit/demotest.java
+++ demogit/src/demogit/demotest.java
@@ -5,6 +5,8 @@ public class demotest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("first comit");
+		//2nd line
+		System.out.println("2nd comit");
 	}
 
 }
